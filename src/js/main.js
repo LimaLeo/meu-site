ed72f94c171e526e2e86716aa43a97628ef6b9b2
Leonardo Lima <leonardotlima2@gmail.com>
2017-02-24 17:29:13 -0300

    efeito e scroll links menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,42 @@
 $(function(){
-	var nav = $('.fixed');   
+	var nav = $('header');   
 	$(window).scroll(function () { 
 		if ($(this).scrollTop() >= 80) { 
-			nav.fadeIn("slow");
+			nav.addClass("fixed");
 		} else{ 
-			nav.stop().fadeOut("slow");
+			nav.removeClass("fixed");
 		} 
 	});  
+});
 
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
 });
